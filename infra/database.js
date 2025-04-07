ed2a816fe046bb073994f24e5b9bab9b107f961a
sjunqueira <sergiojunqueira.s@gmail.com>
2025-04-07 19:29:10 -0300

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import { Client } from 'pg';
+
+async function connect() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
+  await client.connect();
+  return client;
+}
+
+async function query(queryObject) {
+  const client = await connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+
+export default {
+  query
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ "mensagem": "Deu certo, é show demais" }); //Deu tudo certo
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as Soma, 2*3 as Multiplicacao;");
+  // console.log(result.rows);
+  response.status(200).json({ "resultado": result.rows }); //Deu tudo certo
 }
 
 export default status;
