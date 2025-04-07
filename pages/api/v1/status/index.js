9551629f2d808fa07a2e94e4a9bb6bb6c73b57bd
sjunqueira <sergiojunqueira.s@gmail.com>
2025-04-07 20:41:09 -0300

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as Soma, 2*3 as Multiplicacao;");
-  // console.log(result.rows);
+  const result = await database.query("SELECT 1+1 as Soma, 2*4 as Multiplicacao;");
+  console.log(result.rows);
   response.status(200).json({ "resultado": result.rows }); //Deu tudo certo
 }
 
