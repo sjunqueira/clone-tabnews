3b297305a4c4bb064f850ced4c290117c45e055b
sjunqueira <sergiojunqueira.s@gmail.com>
2025-04-13 02:27:00 -0300

    make  more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -14,9 +14,16 @@ async function connect() {
 
 async function query(queryObject) {
   const client = await connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+
 }
 
 
