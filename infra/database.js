54299b8c1526e77c085e2d5f78d0c45a16218309
sjunqueira <sergiojunqueira.s@gmail.com>
2025-04-19 13:49:47 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,15 +6,8 @@ async function connect() {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  });
-
-  console.log('Credenciais do Postgres', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true
   });
   return client;
 }
